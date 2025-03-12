76b2990834aca9f10033e427fb8a833f46861753
sethdaking <lastemail768@gmail.com>
2025-03-12 17:10:53 +0200

    Reducing the time the API uses

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++
@@ -1,8 +0,0 @@
-import { SignIn } from '@clerk/nextjs'
-
-export default function Page() {
-    <div className='flex items-center justify-center h-screen'>
-        <SignIn />
-    </div>
-  return 
-}
