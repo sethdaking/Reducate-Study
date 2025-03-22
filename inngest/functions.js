582141b0f62097bfe9cabf03b8dd09428db3ac67
sethdaking <lastemail768@gmail.com>
2025-03-22 15:07:15 +0200

    AI is not replacing us soon

--- inngest/functions.js
+++ inngest/functions.js
@@ -204,11 +204,12 @@ export const GenerateStudyTypeContent = inngest.createFunction(
             7. Does not use "Question:" and "Explanation:" prefixes with emphasis
             8. Minimum of 25 Questions
             9. Does not include the letters in the text
-            10. Does not include the letters(A-D in the text)
+            10. Does not include the letters(A-D in the text at the beginning of the answer)
             11. Uses <strong className='text-xl'> for emphasis in the question and anwer
             12. Make the questions as challenging as possible
             13. Make the options nuanced with subtle differences to increase difficulty and understanding
-            13. Make the questions REALLY HARD but based on the material provided
+            14. Make the questions REALLY HARD but based on the material provided
+            15. Make sure the text is readable with no spelling errors or grammatical errors
           `;
           response = await GenerateQuiz.sendMessage(prompt);
         } else if (studyType === "qna") {
